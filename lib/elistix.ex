bb50ba24f7f19fef9b527eb0df28bb7c4cbb41c6
lcostantini <costantinileandro1@gmail.com>
2016-11-23 13:23:46 -0300

    Add Elistix module.

--- lib/elistix.ex
+++ lib/elistix.ex
@@ -1,2 +1,27 @@
 defmodule Elistix do
+  @moduledoc """
+  Provides a configuration base for the other modules.
+
+  Here you can find methods used by HTTPoison to configure the URL, headers
+  and body (you can read the doc to get more info - https://github.com/edgurgel/httpoison/blob/master/lib/httpoison/base.ex).
+
+  If you want to change any of this behavior you need to override the functions
+  in your code.
+  """
+
+  use HTTPoison.Base
+
+  @endpoint System.get_env("ES_URI") || "http://127.0.0.1:9200"
+
+  defp process_url(url) do
+    @endpoint <> url
+  end
+
+  defp process_response_body(body) do
+    body |> Poison.decode!
+  end
+
+  defp process_request_headers(headers) do
+    [{"content-type", "application/json"} | headers]
+  end
 end
