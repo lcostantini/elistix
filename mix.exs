2f5c6541c4fcc7022df61cfb4ed2c4566374c6ad
lcostantini <costantinileandro1@gmail.com>
2016-12-01 14:55:30 -0300

    Change poison version to work with Phoenix.

--- mix.exs
+++ mix.exs
@@ -28,6 +28,6 @@ defmodule Elistix.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:httpoison, "~> 0.10.0"},
-     {:poison, "~> 3.0"}]
+     {:poison, "~> 2.0"}]
   end
 end
