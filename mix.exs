1244d848dc04900763012675e8020a79ef844083
lcostantini <costantinileandro1@gmail.com>
2016-12-07 15:45:29 -0300

    Update mix.exs file

--- mix.exs
+++ mix.exs
@@ -7,25 +7,30 @@ defmodule Elistix.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp description do
+    """
+    Use Elasticsearch along with your Elixir app without learn any new DSL.
+    """
+  end
+
+  defp package do
+    [
+      name: "Elistix",
+      maintainers: ["Leandro Costantini"],
+      licenses: ["GPL 3.0"],
+      links: %{"GitHub" => "https://github.com/lcostantini/elistix"}
+    ]
+  end
+
   defp deps do
     [{:httpoison, "~> 0.10.0"},
      {:poison, "~> 2.0"}]
